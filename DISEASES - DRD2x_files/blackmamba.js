412e835b70cc53fa7b74dd0c1e537434801bd5ef
Chris Marino <chris@promptone.ai>
2025-02-11 20:11:07 -0800

    Update

--- DISEASES - DRD2x_files/blackmamba.js
+++ DISEASES - DRD2x_files/blackmamba.js
@@ -1,7 +1,6 @@
 // =============================================================================
 // Ajax stuff.
 // =============================================================================
-let PAGESIZE = 250;
 function blackmamba_xmlhttp()
 {
 	if (window.XMLHttpRequest) {
@@ -103,21 +102,21 @@ function blackmamba_cyclebase_timecourse_request(xmlhttp, url, query, container)
 function blackmamba_search(url, section, limit, page, container)
 {
 	div = document.getElementById(container);
-	div.innerHTML = "<H3>Searching</H3><p>Fetching results for page "PAGESIZE+". Please wait ...</p>";
+	div.innerHTML = "<H3>Searching</H3><p>Fetching results for page "+page+". Please wait ...</p>";
 	form = document.blackmamba_search_form;
 	query = "query="+escape(form.query.value);
 	window.history.pushState(null, null, window.location.href.split("?")[0]+"?"+query);
 	if (section != "") {
 		query += "&section="+section
 	}
-	query += "&limit="+limit+"&page="PAGESIZE+"&container="+container;
+	query += "&limit="+limit+"&page="+page+"&container="+container;
 	blackmamba_request(blackmamba_xmlhttp(), url, query, container);
 }
 
 function blackmamba_advanced_search(url, section, limit, page, container)
 {
 	div = document.getElementById(container)
-	div.innerHTML = "<H3>Searching</H3><p>Fetching results for page "PAGESIZE+". Please wait ...</p>"
+	div.innerHTML = "<H3>Searching</H3><p>Fetching results for page "+page+". Please wait ...</p>"
 	form = document.blackmamba_advanced_search_form;
 	query = ""
 	id = "id="+escape(form.id.value)
@@ -125,7 +124,7 @@ function blackmamba_advanced_search(url, section, limit, page, container)
 	phase  = "phase="+escape(form.phase.value)
 	rank  = "rank="+escape(form.rank.value)
 	phenotype = "phenotype="+escape(form.phenotype.value)
-	query += id+"&"+type+"&"+phase+"&"+phenotype+"&"+rank+"&limit="+limit+"&page="PAGESIZE+"&container="+container
+	query += id+"&"+type+"&"+phase+"&"+phenotype+"&"+rank+"&limit="+limit+"&page="+page+"&container="+container
 	if (section != "") {
 		query += "&section="+section
 	}
@@ -147,7 +146,7 @@ function blackmamba_pager(url, query, limit, page, container)
 	div = document.getElementById(container);
 	div.innerHTML = '<p>Fetching ...</p>';
 	div.style.visibility = 'visible';
-	blackmamba_request(blackmamba_xmlhttp(), url, query+"&limit="+limit+"&page="PAGESIZE+"&container="+escape(container), container);
+	blackmamba_request(blackmamba_xmlhttp(), url, query+"&limit="+limit+"&page="+page+"&container="+escape(container), container);
 }
 
 function blackmamba_timecourses(query, container,name,visible,async)
---
+++ README.md
@@ -0,0 +1,8 @@
+# Static pages from Diseases database
+
+Hardcode page size to 250. 
+1. Clone this repo
+`git clone https://github.com/chrismarino/george.git`
+2. Open html page
+    file://<local-path>/diseases/DISC1.html
+    file://<local-path>/diseases/DRD2.html
